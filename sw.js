a122fdb0bee4a2d44a73978eb04b054b012497f8
bbelchioravac-design <bbelchior.avac@gmail.com>
2026-06-09 10:59:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'alios-calculos-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './splash.png',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './css/styles.css',
+  './js/app.js',
+  './js/avac.js',
+  './js/incendio.js',
+  './data/quadro2_actividades.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
